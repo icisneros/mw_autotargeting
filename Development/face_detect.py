ff391550b9370200ab3d6a3b7c20e42e2bf60ca3
Ivan Cisneros <ivanalexiscisneros@college.harvard.edu>
2017-03-20 22:14:07 -0700

    filepath changed

--- Development/face_detect.py
+++ Development/face_detect.py
@@ -19,7 +19,7 @@ buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
 image = cv2.imdecode(buff, 1)
 
 #Load a cascade file for detecting faces
-face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
+face_cascade = cv2.CascadeClassifier('/home/pi/Desktop/mw_autotargeting/Development/haarcascade_frontalface_alt.xml')
 
 #Convert to grayscale
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
